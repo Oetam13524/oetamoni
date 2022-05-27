62878ab3f17a9dafdaffd63eb78b639eca6825da
Mateo <mateo@gmail.com>
2022-05-27 11:13:04 +0200

    Added model Comment

--- design/models.py
+++ design/models.py
@@ -17,3 +17,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=40)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=150)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
