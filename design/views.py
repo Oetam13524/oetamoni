1fb808ee689058bde99159e4102b64dd8dd26f54
Mateo <mateo@gmail.com>
2022-05-31 11:45:46 +0200

    Added LoginPage

--- design/views.py
+++ design/views.py
@@ -83,3 +83,23 @@ def registerPage(request):
 
 		context = {'form':form}
 		return render(request, 'design/register.html', context)
+
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('starting-page')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('starting-page')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'design/login.html', context)
