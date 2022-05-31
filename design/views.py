f75f38f07d5ef107467878cd5e95d0fee0ff5af2
Mateo <mateo@gmail.com>
2022-05-31 11:44:59 +0200

    Added RegisterPage

--- design/views.py
+++ design/views.py
@@ -64,3 +64,22 @@ class PostDetailView(View):
         }
 
         return render(request, "design/post-detail.html", context)
+
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('starting-page')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'design/register.html', context)
